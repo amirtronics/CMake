c25b06cc4ee3ef037de265a04d844aef70a2574e
legatus <amir.h.ebrahimnezhad@gmail.com>
2023-10-11 14:22:47 -0700

    Add Baumer

---
+++ Baumer/opencv.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+#include <opencv2/highgui.hpp>
+#include "neoapi/neoapi.hpp"
+
+
+int main() {
+    int result = 0;
+    std::cout << "OpenCV Executed!\n";
+
+    return result;
+}
+
---
+++ Baumer/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cd out/build ; ./opencv_cpp
